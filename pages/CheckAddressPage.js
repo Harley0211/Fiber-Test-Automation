bbdf81b87e65f55926cfc6fce94b3a4f43bf0979
Harley0211 <harleydavevalencia@gmail.com>
2026-06-05 12:15:17 -0400

    updated code as of now

--- pages/CheckAddressPage.js
+++ pages/CheckAddressPage.js
@@ -1,49 +1,7 @@
-import { expect } from "@playwright/test";
+export async function checkAddress(name) {
+  await page.goto("https://qlab02.fiber.t-mobile.com/check-address");
 
-export class CheckAddressPage {
-  constructor(page) {
-    this.page = page;
+  await page.locator("#address");
 
-    //address input
-    this.addressInput = page.locator('[name="Address"]');
-
-    //address suggestion
-    this.addressSuggestionDropdown = page.locator(
-      "#address-dropdown-suggestions-list",
-    );
-    this.addressSuggestions = page.locator(
-      "#address-dropdown-suggestions-list li",
-    );
-
-    //unit number lists
-    this.unitDropdown = page.locator('[name = "unit"]');
-    this.unitListBox = page.getByRole("listbox");
-    this.unitOption = page.getByRole("option");
-
-    //next btn
-    this.nextBtn = page.getByRole("button", {
-      name: "Next",
-    });
-  }
-
-  async goto() {
-    await this.page.goto("https://qlab02.fiber.t-mobile.com/check-address");
-  }
-  async searchAddress(address) {
-    await this.addressInput.fill(address);
-  }
-
-  async selectFirstAddressSuggestion() {
-    await expect(this.addressSuggestionDropdown).toBeVisible();
-
-    await this.addressSuggestions.first().click();
-  }
-
-  async selectFirstUnit() {
-    await this.unitDropdown.click();
-
-    await expect(this.unitListBox).toBeVisible();
-
-    await this.unitOption.first().click();
-  }
+  await page.locator("option");
 }
--- tests/unassisted.spec.js
+++ tests/unassisted.spec.js
@@ -1,15 +1,21 @@
 import { test, expect } from "@playwright/test";
 
 test("Unassisted Prospect Flow", async ({ page }) => {
+  const qlab02 = "https://qlab02.fiber.t-mobile.com/check-address";
+
   //check-address page
 
   //check-address field
-  await page.goto("https://qlab02.fiber.t-mobile.com/check-address");
+  await page.goto(qlab02);
   await page.locator("#address").fill("61 delancey");
   await expect(page.getByRole("listbox")).toBeVisible();
   await page.getByRole("option").first().click();
 
   // unit no. selection
   await expect(page.locator("#fca-unit")).toBeVisible();
-  await page.getByRole("combobox").first();
+  await page.getByRole("combobox").click();
+  await page.getByRole("option").first().click();
+  await page.getByTestId("fca-btn").click();
+
+  // offering page
 });
