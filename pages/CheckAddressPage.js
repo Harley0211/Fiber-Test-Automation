21ef327f5e735e8d47e618a40c7dc45d903f2338
Harley0211 <harleydavevalencia@gmail.com>
2026-06-05 18:32:16 -0400

    updated code weekend

--- pages/CheckAddressPage.js
+++ pages/CheckAddressPage.js
@@ -1,7 +1,13 @@
 export async function checkAddress(name) {
   await page.goto("https://qlab02.fiber.t-mobile.com/check-address");
 
-  await page.locator("#address");
+  await page.locator("#address").fill("61 delancey");
 
-  await page.locator("option");
+  await page.locator("option").first().click();
+
+  await page.getByRole("combobox").click();
+
+  await page.getByRole("option").first().click();
+
+  await page.getByTesId("fca-btn").click();
 }
--- tests/unassisted.spec.js
+++ tests/unassisted.spec.js
@@ -1,21 +1,6 @@
 import { test, expect } from "@playwright/test";
+import { checkAddress } from "../pages/CheckAddressPage";
 
 test("Unassisted Prospect Flow", async ({ page }) => {
-  const qlab02 = "https://qlab02.fiber.t-mobile.com/check-address";
-
-  //check-address page
-
-  //check-address field
-  await page.goto(qlab02);
-  await page.locator("#address").fill("61 delancey");
-  await expect(page.getByRole("listbox")).toBeVisible();
-  await page.getByRole("option").first().click();
-
-  // unit no. selection
-  await expect(page.locator("#fca-unit")).toBeVisible();
-  await page.getByRole("combobox").click();
-  await page.getByRole("option").first().click();
-  await page.getByTestId("fca-btn").click();
-
-  // offering page
+  await checkAddress(page);
 });
